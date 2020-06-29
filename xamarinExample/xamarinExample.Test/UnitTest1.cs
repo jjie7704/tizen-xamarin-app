d86a32a0dbc5f04ba4ac46712a86ea8ce6dc061d
msaltnet <salt.jeong@gmail.com>
2020-06-30 00:48:20 +0900

    Create projects

---
+++ xamarinExample/xamarinExample.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace xamarinExample.test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
